95cbcdfd8a1d51ab4a90a359f78a546dc664722a
BuildTools <unconfigured@null.spigotmc.org>
2024-01-04 16:15:15 -0500

    First Commit

--- src/main/java/net/lettuce/metalworks/MetalWorks.java
+++ src/main/java/net/lettuce/metalworks/MetalWorks.java
@@ -1,21 +1,21 @@
 package net.lettuce.metalworks;
 
 import com.mojang.logging.LogUtils;
+import net.lettuce.metalworks.item.custom.ModItems;
 import net.minecraft.client.Minecraft;
-import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.ForgeRegistries;
 import org.slf4j.Logger;
 
 @Mod(MetalWorks.MOD_ID)
@@ -27,6 +27,8 @@ public class MetalWorks
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModItems.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
@@ -35,12 +37,53 @@ public class MetalWorks
 
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event)
-    {
+    private void commonSetup(final FMLCommonSetupEvent event) {
 
     }
 
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.TIN_INGOT);
+            event.accept(ModItems.BRONZE_INGOT);
+            event.accept(ModItems.ROSE_GOLD_INGOT);
+            event.accept(ModItems.RAW_TIN);
+            event.accept(ModItems.TIN_NUGGET);
+            event.getEntries().putAfter(Items.COPPER_INGOT.getDefaultInstance(), ModItems.TIN_INGOT.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+            event.getEntries().putBefore(Items.GOLD_INGOT.getDefaultInstance(), ModItems.BRONZE_INGOT.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+            event.getEntries().putAfter(Items.GOLD_INGOT.getDefaultInstance(), ModItems.ROSE_GOLD_INGOT.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+            event.getEntries().putAfter(Items.RAW_COPPER.getDefaultInstance(), ModItems.RAW_TIN.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+            event.getEntries().putAfter(Items.IRON_NUGGET.getDefaultInstance(), ModItems.TIN_NUGGET.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+
+            event.getEntries().putAfter(Items.RAW_GOLD.getDefaultInstance(), Items.NETHERITE_SCRAP.getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+        }
+
+        if(event.getTabKey() == CreativeModeTabs.FOOD_AND_DRINKS) {
+            event.accept(ModItems.ROSE_GOLD_APPLE);
+            event.accept(ModItems.ROSE_GOLD_BEETROOT);
+            event.getEntries().putAfter(Items.GOLDEN_APPLE.getDefaultInstance(), ModItems.ROSE_GOLD_APPLE.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+            event.getEntries().putAfter(Items.BEETROOT.getDefaultInstance(), ModItems.ROSE_GOLD_BEETROOT.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+
+        }
+
+        if(event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
+            event.accept(ModItems.BRONZE_SHOVEL);
+            event.accept(ModItems.BRONZE_PICKAXE);
+            event.accept(ModItems.BRONZE_AXE);
+            event.accept(ModItems.BRONZE_HOE);
+            event.accept(ModItems.BRONZE_SHEARS);
+            event.getEntries().putAfter(Items.IRON_HOE.getDefaultInstance(), ModItems.BRONZE_SHOVEL.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+            event.getEntries().putAfter(Items.SHEARS.getDefaultInstance(), ModItems.BRONZE_SHEARS.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+            event.getEntries().putAfter(ModItems.BRONZE_SHOVEL.get().getDefaultInstance(), ModItems.BRONZE_PICKAXE.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+            event.getEntries().putAfter(ModItems.BRONZE_PICKAXE.get().getDefaultInstance(), ModItems.BRONZE_AXE.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+            event.getEntries().putAfter(ModItems.BRONZE_AXE.get().getDefaultInstance(), ModItems.BRONZE_HOE.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+
+            if(event.getTabKey() == CreativeModeTabs.COMBAT) {
+                event.accept(ModItems.BRONZE_SWORD);
+                event.accept(ModItems.BRONZE_AXE);
+                event.getEntries().putAfter(Items.IRON_SWORD.getDefaultInstance(), ModItems.BRONZE_SWORD.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+                event.getEntries().putAfter(ModItems.BRONZE_SWORD.get().getDefaultInstance(), ModItems.BRONZE_AXE.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+            }
+        }
 
     }
 
---
+++ src/main/java/net/lettuce/metalworks/item/custom/ModItems.java
@@ -0,0 +1,85 @@
+package net.lettuce.metalworks.item.custom;
+
+import net.lettuce.metalworks.MetalWorks;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, MetalWorks.MOD_ID);
+
+    public static final RegistryObject<Item> TIN_INGOT = ITEMS.register("tin_ingot",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> TIN_SWORD = ITEMS.register("tin_sword",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> TIN_PICKAXE = ITEMS.register("tin_pickaxe",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> TIN_AXE = ITEMS.register("tin_axe",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> TIN_SHOVEL = ITEMS.register("tin_shovel",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> TIN_HOE = ITEMS.register("tin_hoe",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> BRONZE_INGOT = ITEMS.register("bronze_ingot",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> BRONZE_SWORD = ITEMS.register("bronze_sword",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> BRONZE_PICKAXE = ITEMS.register("bronze_pickaxe",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> BRONZE_AXE = ITEMS.register("bronze_axe",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> BRONZE_SHOVEL = ITEMS.register("bronze_shovel",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> BRONZE_HOE = ITEMS.register("bronze_hoe",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> BRONZE_SHEARS = ITEMS.register("bronze_shears",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> ROSE_GOLD_INGOT = ITEMS.register("rose_gold_ingot",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> ROSE_GOLD_SWORD = ITEMS.register("rose_gold_sword",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> ROSE_GOLD_PICKAXE = ITEMS.register("rose_gold_pickaxe",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> ROSE_GOLD_AXE = ITEMS.register("rose_gold_axe",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> ROSE_GOLD_SHOVEl = ITEMS.register("rose_gold_shovel",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> ROSE_GOLD_HOE = ITEMS.register("rose_gold_hoe",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> RAW_TIN = ITEMS.register("raw_tin",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> TIN_NUGGET = ITEMS.register("tin_nugget",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> ROSE_GOLD_APPLE = ITEMS.register("rose_gold_apple",
+            () -> new Item(new Item.Properties().food(ModFoods.ROSE_GOLD_APPLE)));
+    public static final RegistryObject<Item> ROSE_GOLD_BEETROOT = ITEMS.register("rose_gold_beetroot",
+            () -> new Item(new Item.Properties().food(ModFoods.ROSE_GOLD_BEETROOT)));
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
