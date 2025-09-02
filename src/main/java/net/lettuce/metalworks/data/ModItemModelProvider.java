249eedb8135334700b345805ec20ed9243f2d00d
BuildTools <unconfigured@null.spigotmc.org>
2025-09-01 23:48:47 -0400

    commit just incase i break things again

--- src/main/java/net/lettuce/metalworks/data/ModItemModelProvider.java
+++ src/main/java/net/lettuce/metalworks/data/ModItemModelProvider.java
@@ -5,16 +5,37 @@ import net.lettuce.metalworks.core.MetalWorks;
 import net.lettuce.metalworks.registry.MWBlocks;
 import net.lettuce.metalworks.registry.MWItems;
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.armortrim.TrimMaterial;
+import net.minecraft.world.item.armortrim.TrimMaterials;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.client.model.generators.ItemModelBuilder;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraftforge.client.model.generators.ModelFile;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+import java.util.LinkedHashMap;
+
 public class ModItemModelProvider extends ItemModelProvider {
+    private static LinkedHashMap<ResourceKey<TrimMaterial>, Float> trimMaterials = new LinkedHashMap<>();
+    static {
+        trimMaterials.put(TrimMaterials.QUARTZ, 0.1F);
+        trimMaterials.put(TrimMaterials.IRON, 0.2F);
+        trimMaterials.put(TrimMaterials.NETHERITE, 0.3F);
+        trimMaterials.put(TrimMaterials.REDSTONE, 0.4F);
+        trimMaterials.put(TrimMaterials.COPPER, 0.5F);
+        trimMaterials.put(TrimMaterials.GOLD, 0.6F);
+        trimMaterials.put(TrimMaterials.EMERALD, 0.7F);
+        trimMaterials.put(TrimMaterials.DIAMOND, 0.8F);
+        trimMaterials.put(TrimMaterials.LAPIS, 0.9F);
+        trimMaterials.put(TrimMaterials.AMETHYST, 1.0F);
+    }
     public ModItemModelProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
         super(output, MetalWorks.MOD_ID, existingFileHelper);
     }
@@ -24,10 +45,10 @@ public class ModItemModelProvider extends ItemModelProvider {
          simpleItem(MWItems.TIN_INGOT);
          simpleItem(MWItems.TIN_NUGGET);
          simpleItem(MWItems.RAW_TIN);
-        simpleItem(MWItems.TIN_HELMET);
-        simpleItem(MWItems.TIN_CHESTPLATE);
-        simpleItem(MWItems.TIN_LEGGINGS);
-        simpleItem(MWItems.TIN_BOOTS);
+        trimmedArmorItem(MWItems.TIN_HELMET);
+        trimmedArmorItem(MWItems.TIN_CHESTPLATE);
+        trimmedArmorItem(MWItems.TIN_LEGGINGS);
+        trimmedArmorItem(MWItems.TIN_BOOTS);
         simpleItem(MWItems.TIN_HORSE_ARMOR);
         handheldItem(MWItems.TIN_SWORD);
         handheldItem(MWItems.TIN_PICKAXE);
@@ -37,10 +58,10 @@ public class ModItemModelProvider extends ItemModelProvider {
 
          simpleItem(MWItems.BRONZE_INGOT);
          simpleItem(MWItems.BRONZE_NUGGET);
-         simpleItem(MWItems.BRONZE_HELMET);
-         simpleItem(MWItems.BRONZE_CHESTPLATE);
-         simpleItem(MWItems.BRONZE_LEGGINGS);
-         simpleItem(MWItems.BRONZE_BOOTS);
+        trimmedArmorItem(MWItems.BRONZE_HELMET);
+        trimmedArmorItem(MWItems.BRONZE_CHESTPLATE);
+        trimmedArmorItem(MWItems.BRONZE_LEGGINGS);
+        trimmedArmorItem(MWItems.BRONZE_BOOTS);
          simpleItem(MWItems.BRONZE_HORSE_ARMOR);
          handheldItem(MWItems.BRONZE_SWORD);
          handheldItem(MWItems.BRONZE_PICKAXE);
@@ -51,11 +72,12 @@ public class ModItemModelProvider extends ItemModelProvider {
 
         simpleItem(MWItems.ROSE_GOLD_INGOT);
         simpleItem(MWItems.ROSE_GOLD_NUGGET);
-        simpleItem(MWItems.ROSE_GOLD_HELMET);
-        simpleItem(MWItems.ROSE_GOLD_CHESTPLATE);
-        simpleItem(MWItems.ROSE_GOLD_LEGGINGS);
+        trimmedArmorItem(MWItems.ROSE_GOLD_HELMET);
+        trimmedArmorItem(MWItems.ROSE_GOLD_CHESTPLATE);
+        trimmedArmorItem(MWItems.ROSE_GOLD_LEGGINGS);
+        trimmedArmorItem(MWItems.ROSE_GOLD_BOOTS);
         simpleItem(MWItems.ROSE_GOLD_HORSE_ARMOR);
-        simpleItem(MWItems.ROSE_GOLD_BOOTS);
+
         simpleItem(MWItems.ROSE_GOLD_BEETROOT);
         handheldItem(MWItems.ROSE_GOLD_SWORD);
         handheldItem(MWItems.ROSE_GOLD_PICKAXE);
@@ -252,4 +274,54 @@ public class ModItemModelProvider extends ItemModelProvider {
                 new ResourceLocation("item/generated")).texture("layer0",
                 new ResourceLocation(MetalWorks.MOD_ID,"item/" + item.getId().getPath()));
     }
+
+    // Shoutout to El_Redstoniano for making this
+    // Shoutout to Kaupenjoe for showing me this
+    private void trimmedArmorItem(RegistryObject<Item> itemRegistryObject) {
+        final String MOD_ID = MetalWorks.MOD_ID; // Change this to your mod id
+
+        if(itemRegistryObject.get() instanceof ArmorItem armorItem) {
+            trimMaterials.entrySet().forEach(entry -> {
+
+                ResourceKey<TrimMaterial> trimMaterial = entry.getKey();
+                float trimValue = entry.getValue();
+
+                String armorType = switch (armorItem.getEquipmentSlot()) {
+                    case HEAD -> "helmet";
+                    case CHEST -> "chestplate";
+                    case LEGS -> "leggings";
+                    case FEET -> "boots";
+                    default -> "";
+                };
+
+                String armorItemPath = "item/" + armorItem;
+                String trimPath = "trims/items/" + armorType + "_trim_" + trimMaterial.location().getPath();
+                String currentTrimName = armorItemPath + "_" + trimMaterial.location().getPath() + "_trim";
+                ResourceLocation armorItemResLoc = new ResourceLocation(MOD_ID, armorItemPath);
+                ResourceLocation trimResLoc = new ResourceLocation(trimPath); // minecraft namespace
+                ResourceLocation trimNameResLoc = new ResourceLocation(MOD_ID, currentTrimName);
+
+                // This is used for making the ExistingFileHelper acknowledge that this texture exist, so this will
+                // avoid an IllegalArgumentException
+                existingFileHelper.trackGenerated(trimResLoc, PackType.CLIENT_RESOURCES, ".png", "textures");
+
+                // Trimmed armorItem files
+                getBuilder(currentTrimName)
+                        .parent(new ModelFile.UncheckedModelFile("item/generated"))
+                        .texture("layer0", armorItemResLoc)
+                        .texture("layer1", trimResLoc);
+
+                // Non-trimmed armorItem file (normal variant)
+                this.withExistingParent(itemRegistryObject.getId().getPath(),
+                                mcLoc("item/generated"))
+                        .override()
+                        .model(new ModelFile.UncheckedModelFile(trimNameResLoc))
+                        .predicate(mcLoc("trim_type"), trimValue).end()
+                        .texture("layer0",
+                                new ResourceLocation(MOD_ID,
+                                        "item/" + itemRegistryObject.getId().getPath()));
+            });
+        }
+    }
+
 }
--- src/main/java/net/lettuce/metalworks/data/ModItemTagGenerator.java
+++ src/main/java/net/lettuce/metalworks/data/ModItemTagGenerator.java
@@ -1,9 +1,11 @@
 package net.lettuce.metalworks.data;
 
 import net.lettuce.metalworks.core.MetalWorks;
+import net.lettuce.metalworks.registry.MWItems;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
@@ -18,6 +20,26 @@ public class ModItemTagGenerator extends ItemTagsProvider {
 
     @Override
     protected void addTags(HolderLookup.Provider pProvider) {
+
+        // Trimmable Armor
+        this.tag(ItemTags.TRIMMABLE_ARMOR)
+                .add(MWItems.TIN_HELMET.get(),
+                MWItems.TIN_CHESTPLATE.get(),
+                MWItems.TIN_LEGGINGS.get(),
+                MWItems.TIN_BOOTS.get(),
+                MWItems.BRONZE_HELMET.get(),
+                MWItems.BRONZE_CHESTPLATE.get(),
+                MWItems.BRONZE_LEGGINGS.get(),
+                MWItems.BRONZE_BOOTS.get(),
+                MWItems.ROSE_GOLD_HELMET.get(),
+                MWItems.ROSE_GOLD_CHESTPLATE.get(),
+                MWItems.ROSE_GOLD_LEGGINGS.get(),
+                MWItems.ROSE_GOLD_BOOTS.get());
+
+        this.tag(ItemTags.TRIM_MATERIALS)
+                .add(MWItems.TIN_INGOT.get(),
+                MWItems.BRONZE_INGOT.get(),
+                MWItems.ROSE_GOLD_INGOT.get());
 
     }
 }
