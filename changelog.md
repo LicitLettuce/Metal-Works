3efc98a4f1d65aaa3020f37d14c4e2c6e0763ed2
BuildTools <unconfigured@null.spigotmc.org>
2026-06-07 23:25:24 -0400

    Soo umm.. Enchantments need tags now!

--- changelog.md
+++ changelog.md
@@ -5,8 +5,6 @@
 - Mage campfire no longer crashes the game
 - Mage campfire now actually cooks food
 - Suspicous soul soil no longer crashes the game
-- Lost Citadel now generates properly
-- Ethos and Tarnish armor trims now work as intended
 
 ## 1.2.12 - 1.21.1
 ### Changes
--- src/main/java/net/lettuce/metalworks/datagen/ModItemTagGenerator.java
+++ src/main/java/net/lettuce/metalworks/datagen/ModItemTagGenerator.java
@@ -43,5 +43,179 @@ public class ModItemTagGenerator extends ItemTagsProvider {
         this.tag(ItemTags.TRIM_TEMPLATES)
                 .add(ModItems.ETHOS_ARMOR_TRIM_SMITHING_TEMPLATE.get(),
                         ModItems.TARNISH_ARMOR_TRIM_SMITHING_TEMPLATE .get());
+
+        this.tag(ItemTags.ARMOR_ENCHANTABLE)
+                .add(ModItems.TIN_HELMET.get(),
+                        ModItems.TIN_CHESTPLATE .get(),
+                        ModItems.TIN_LEGGINGS .get(),
+                        ModItems.TIN_BOOTS .get(),
+                        ModItems.BRONZE_HELMET .get(),
+                        ModItems.BRONZE_CHESTPLATE .get(),
+                        ModItems.BRONZE_LEGGINGS .get(),
+                        ModItems.BRONZE_BOOTS .get(),
+                        ModItems.ROSE_GOLD_HELMET .get(),
+                        ModItems.ROSE_GOLD_CHESTPLATE .get(),
+                        ModItems.ROSE_GOLD_LEGGINGS .get(),
+                        ModItems.ROSE_GOLD_BOOTS .get());
+
+        this.tag(ItemTags.BREAKS_DECORATED_POTS)
+                .add(ModItems.TIN_SWORD.get(),
+                        ModItems.TIN_SHOVEL .get(),
+                        ModItems.TIN_AXE .get(),
+                        ModItems.TIN_PICKAXE .get(),
+                        ModItems.TIN_HOE .get(),
+                        ModItems.BRONZE_SWORD .get(),
+                        ModItems.BRONZE_SHOVEL .get(),
+                        ModItems.BRONZE_PICKAXE .get(),
+                        ModItems.BRONZE_AXE .get(),
+                        ModItems.BRONZE_HOE.get(),
+                        ModItems.ROSE_GOLD_SWORD .get(),
+                        ModItems.ROSE_GOLD_SHOVEL .get(),
+                        ModItems.ROSE_GOLD_PICKAXE .get(),
+                        ModItems.ROSE_GOLD_HOE .get(),
+                        ModItems.ROSE_GOLD_AXE .get());
+
+        this.tag(ItemTags.MINING_ENCHANTABLE)
+                .add(ModItems.TIN_HOE.get(),
+                        ModItems.TIN_SHOVEL .get(),
+                        ModItems.TIN_AXE .get(),
+                        ModItems.TIN_PICKAXE .get(),
+                        ModItems.BRONZE_SHOVEL .get(),
+                        ModItems.BRONZE_PICKAXE .get(),
+                        ModItems.BRONZE_AXE .get(),
+                        ModItems.BRONZE_HOE.get(),
+                        ModItems.ROSE_GOLD_SHOVEL .get(),
+                        ModItems.ROSE_GOLD_PICKAXE .get(),
+                        ModItems.ROSE_GOLD_HOE .get(),
+                        ModItems.ROSE_GOLD_AXE .get(),
+                        ModItems.BRONZE_SHEARS .get());
+
+        this.tag(ItemTags.MINING_LOOT_ENCHANTABLE)
+                .add(ModItems.TIN_HOE.get(),
+                        ModItems.TIN_SHOVEL .get(),
+                        ModItems.TIN_AXE .get(),
+                        ModItems.TIN_PICKAXE .get(),
+                        ModItems.BRONZE_SHOVEL .get(),
+                        ModItems.BRONZE_PICKAXE .get(),
+                        ModItems.BRONZE_AXE .get(),
+                        ModItems.BRONZE_HOE.get(),
+                        ModItems.ROSE_GOLD_SHOVEL .get(),
+                        ModItems.ROSE_GOLD_PICKAXE .get(),
+                        ModItems.ROSE_GOLD_HOE .get(),
+                        ModItems.ROSE_GOLD_AXE .get());
+
+
+        this.tag(ItemTags.WEAPON_ENCHANTABLE)
+                .add(ModItems.TIN_SWORD.get(),
+                        ModItems.TIN_AXE .get(),
+                        ModItems.BRONZE_SWORD .get(),
+                        ModItems.BRONZE_AXE .get(),
+                        ModItems.ROSE_GOLD_SWORD .get(),
+                        ModItems.ROSE_GOLD_AXE .get());
+
+        this.tag(ItemTags.SHARP_WEAPON_ENCHANTABLE)
+                .add(ModItems.TIN_SWORD.get(),
+                        ModItems.TIN_AXE .get(),
+                        ModItems.BRONZE_SWORD .get(),
+                        ModItems.BRONZE_AXE .get(),
+                        ModItems.ROSE_GOLD_SWORD .get(),
+                        ModItems.ROSE_GOLD_AXE .get());
+
+
+        this.tag(ItemTags.SWORDS)
+                .add(ModItems.TIN_SWORD.get(),
+                        ModItems.BRONZE_SWORD .get(),
+                        ModItems.ROSE_GOLD_SWORD .get());
+
+        this.tag(ItemTags.FIRE_ASPECT_ENCHANTABLE)
+                .add(ModItems.TIN_SWORD.get(),
+                        ModItems.BRONZE_SWORD .get(),
+                        ModItems.ROSE_GOLD_SWORD .get());
+
+        this.tag(ItemTags.CLUSTER_MAX_HARVESTABLES)
+                .add(ModItems.TIN_PICKAXE.get(),
+                        ModItems.BRONZE_PICKAXE .get(),
+                        ModItems.ROSE_GOLD_PICKAXE .get());
+
+        this.tag(ItemTags.PICKAXES)
+                .add(ModItems.TIN_PICKAXE.get(),
+                        ModItems.BRONZE_PICKAXE .get(),
+                        ModItems.ROSE_GOLD_PICKAXE .get());
+
+        this.tag(ItemTags.AXES)
+                .add(ModItems.TIN_AXE.get(),
+                        ModItems.BRONZE_AXE .get(),
+                        ModItems.ROSE_GOLD_AXE .get());
+
+        this.tag(ItemTags.SHOVELS)
+                .add(ModItems.TIN_SHOVEL.get(),
+                        ModItems.BRONZE_SHOVEL .get(),
+                        ModItems.ROSE_GOLD_SHOVEL .get());
+
+        this.tag(ItemTags.HOES)
+                .add(ModItems.TIN_HOE.get(),
+                        ModItems.BRONZE_HOE.get(),
+                        ModItems.ROSE_GOLD_HOE.get());
+
+        this.tag(ItemTags.DURABILITY_ENCHANTABLE)
+                .add(ModItems.TIN_SWORD.get(),
+                        ModItems.TIN_SHOVEL .get(),
+                        ModItems.TIN_AXE .get(),
+                        ModItems.TIN_PICKAXE .get(),
+                        ModItems.TIN_HOE .get(),
+                        ModItems.BRONZE_SWORD .get(),
+                        ModItems.BRONZE_SHOVEL .get(),
+                        ModItems.BRONZE_PICKAXE .get(),
+                        ModItems.BRONZE_AXE .get(),
+                        ModItems.BRONZE_HOE.get(),
+                        ModItems.ROSE_GOLD_SWORD .get(),
+                        ModItems.ROSE_GOLD_SHOVEL .get(),
+                        ModItems.ROSE_GOLD_PICKAXE .get(),
+                        ModItems.ROSE_GOLD_HOE .get(),
+                        ModItems.ROSE_GOLD_AXE .get(),
+                        ModItems.TIN_HELMET.get(),
+                        ModItems.TIN_CHESTPLATE .get(),
+                        ModItems.TIN_LEGGINGS .get(),
+                        ModItems.TIN_BOOTS .get(),
+                        ModItems.BRONZE_HELMET .get(),
+                        ModItems.BRONZE_CHESTPLATE .get(),
+                        ModItems.BRONZE_LEGGINGS .get(),
+                        ModItems.BRONZE_BOOTS .get(),
+                        ModItems.ROSE_GOLD_HELMET .get(),
+                        ModItems.ROSE_GOLD_CHESTPLATE .get(),
+                        ModItems.ROSE_GOLD_LEGGINGS .get(),
+                        ModItems.BRONZE_SHEARS .get(),
+                        ModItems.ROSE_GOLD_BOOTS .get());
+
+        this.tag(ItemTags.VANISHING_ENCHANTABLE)
+                .add(ModItems.TIN_SWORD.get(),
+                        ModItems.TIN_SHOVEL .get(),
+                        ModItems.TIN_AXE .get(),
+                        ModItems.TIN_PICKAXE .get(),
+                        ModItems.TIN_HOE .get(),
+                        ModItems.BRONZE_SWORD .get(),
+                        ModItems.BRONZE_SHOVEL .get(),
+                        ModItems.BRONZE_PICKAXE .get(),
+                        ModItems.BRONZE_AXE .get(),
+                        ModItems.BRONZE_HOE.get(),
+                        ModItems.ROSE_GOLD_SWORD .get(),
+                        ModItems.ROSE_GOLD_SHOVEL .get(),
+                        ModItems.ROSE_GOLD_PICKAXE .get(),
+                        ModItems.ROSE_GOLD_HOE .get(),
+                        ModItems.ROSE_GOLD_AXE .get(),
+                        ModItems.TIN_HELMET.get(),
+                        ModItems.TIN_CHESTPLATE .get(),
+                        ModItems.TIN_LEGGINGS .get(),
+                        ModItems.TIN_BOOTS .get(),
+                        ModItems.BRONZE_HELMET .get(),
+                        ModItems.BRONZE_CHESTPLATE .get(),
+                        ModItems.BRONZE_LEGGINGS .get(),
+                        ModItems.BRONZE_BOOTS .get(),
+                        ModItems.ROSE_GOLD_HELMET .get(),
+                        ModItems.ROSE_GOLD_CHESTPLATE .get(),
+                        ModItems.ROSE_GOLD_LEGGINGS .get(),
+                        ModItems.BRONZE_SHEARS .get(),
+                        ModItems.ROSE_GOLD_BOOTS .get());
+
     }
 }
